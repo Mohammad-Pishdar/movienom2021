2a1309bd1c085e8339b6433ab72b31c42a3ec656
Mo Pishdar <mohpish@gmail.com>
2021-01-08 10:57:54 +1100

    ajax call implemented on click listener and returns correct results

--- script.js
+++ script.js
@@ -2,9 +2,18 @@ const movieSearchBox = $('#movieSearchInputBox');
 const movieSearchButton = $('#movieSearchButton');
 let movieTitle;
 
-movieSearchButton.on('click',function(e) {
+function movieSearch() {
+    $.ajax({
+        url: `http://www.omdbapi.com/?s=${movieTitle}&type=movie&apikey=3b9b8938`,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response);
+    })
+}
+
+movieSearchButton.on('click', function (e) {
     e.preventDefault();
     movieTitle = movieSearchBox.val();
-    console.log(movieTitle);
-} );
+    movieSearch();
+});
 
