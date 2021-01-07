1ebdcd7f01c9f709e8ed687e439432e606ce6e87
Mo Pishdar <mohpish@gmail.com>
2021-01-08 10:11:06 +1100

    getting the value of user input after user clicks the search button

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
-$("#btn").on("click", function() {
-    console.log("clicked!")
-});
+const movieSearchBox = $('#movieSearchInputBox');
+const movieSearchButton = $('#movieSearchButton');
+let movieTitle;
+
+movieSearchButton.on('click',function(e) {
+    e.preventDefault();
+    movieTitle = movieSearchBox.val();
+    console.log(movieTitle);
+} );
+
