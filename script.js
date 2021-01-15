efdf8bb01f5e0bf70d4c8797ea022543ca6a5746
Mo Pishdar <mohpish@gmail.com>
2021-01-16 07:31:56 +1100

    rendered icon element accessible so we can now put an event listener on it to add nominations to the list

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
-const movieSearchBox = $('#movieSearchInputBox');
-const movieSearchButton = $('#movieSearchButton');
-const results = $('.container');
-let movieTitle;
+$(document).ready(function () {
+    //dom not only ready, but everything is loaded
+    const movieSearchBox = $('#movieSearchInputBox');
+    const movieSearchButton = $('#movieSearchButton');
+    const nominationList = $('.nomination-list');
+    const results = $('.container');
+    let heartIcon;
+    let movieTitle;
 
-function movieSearch() {
-    $.ajax({
-        url: `http://www.omdbapi.com/?s=${movieTitle}&type=movie&apikey=3b9b8938`,
-        method: "GET"
-    }).then(function (response) {
-        $.each(response.Search, function (key, value) {
-            if (value.Poster === "N/A") {
-                results.append(`
+    function movieSearch() {
+        $.ajax({
+            url: `http://www.omdbapi.com/?s=${movieTitle}&type=movie&apikey=3b9b8938`,
+            method: "GET"
+        }).then(function (response) {
+            $.each(response.Search, function (key, value) {
+                if (value.Poster === "N/A") {
+                    results.append(`
                 <div class="responsive">
                     <div class="gallery">
                         <img src="https://via.placeholder.com/300x451?text=Image+Not+Found" alt="${value.Title} Image not found" width="300" height="300">
@@ -27,8 +31,8 @@ function movieSearch() {
                         </div>
                     </div>
                 </div>`);
-            } else {
-                results.append(`
+                } else {
+                    results.append(`
                 <div class="responsive">
                     <div class="gallery">
                         <img src=${value.Poster} alt=${value.Title} width="300" height="300">
@@ -45,15 +49,23 @@ function movieSearch() {
                         </div>
                     </div>
                 </div>`);
-            }
-        })
-    })
-}
+                }
+            });
+            heartIcon = $('.icon');
+            console.log(heartIcon);
+            heartIcon.click(function(e) {
+                e.preventDefault();
+                alert('clicked');
+            })
+        }
+        )
+    }
 
-movieSearchButton.on('click', function (e) {
-    e.preventDefault();
-    //ensuring that previous search results disappear as soon as we do a new search
-    results.html("");
-    movieTitle = movieSearchBox.val();
-    movieSearch();
+    movieSearchButton.on('click', function (e) {
+        e.preventDefault();
+        //ensuring that previous search results disappear as soon as we do a new search
+        results.html("");
+        movieTitle = movieSearchBox.val();
+        movieSearch();
+    });
 });
