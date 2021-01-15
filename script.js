3f5b530456c5df02a0ca4aeb76ed28630811be9f
Mo Pishdar <mohpish@gmail.com>
2021-01-16 09:51:23 +1100

    no more than 5 movies will be allowed to be added to the list

--- script.js
+++ script.js
@@ -57,13 +57,17 @@ $(document).ready(function () {
 
             heartIcon.click(function (e) {
                 e.preventDefault();
-                if (jQuery.inArray(e.currentTarget.title, nominationListArray) === -1) {
+                if (jQuery.inArray(e.currentTarget.title, nominationListArray) === -1 && nominationListArray.length < 5) {
                     nominationList.append(`
                 <li>${e.currentTarget.title}<span class="close">&times;</span></li>
                 `)
                     nominationListArray.push(e.currentTarget.title);
                 } else {
                     alert("Item already added to the nomination list.")
+                    return;
+                }
+                if (nominationListArray.length > 5) {
+                    alert("You reached the maximum of 5 nominations. In order to add this item, please remove one of the old items first.");
                     return;
                 }
             });
